82dfcf529b996242277c26c6b475d8eaf91db537
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-27 16:07:36 -0600

    todo app working

--- src/components/todoForm.js
+++ src/components/todoForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 
 function TodoForm(props) {
-const [input, setInput] = useState('')
+const [input, setInput] = useState(props.eidt ? props.edit.value : '');
 
 const inputRef = useRef(null);
 
@@ -27,6 +27,7 @@ const handleSubmit = e => {
     return (
         <div>
             <form className='todo-form' onSubmit = {handleSubmit}>
+
                 <input type='text' 
                 placeholder='Add a todo' 
                 value={input} 
