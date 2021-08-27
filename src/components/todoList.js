300adcedefc7bd0b31b0ffa9e36ba148856d59be
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-27 15:45:36 -0600

    wip

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Todo from './Todo';
 import TodoFrom from './todoForm';
 
 function TodoList() {
@@ -12,13 +13,42 @@ function TodoList() {
         const newTodos = [todo, ...todos]
 
         setTodos(newTodos)
-        console.log(...todos)
+    }
+
+   
+    const editTodo = (todoId, newValue) => {
+        if(!newValue.text || /^\s*$/.test(newValue.text)){
+            return
+        }
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
+    }
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id)
+
+        setTodos(removeArr)
+    }
+
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete
+            }
+            return todo;
+        })
+        setTodos(updatedTodos)
     }
 
     return (
         <div>
            <h1>Whats the plan for today</h1>
-           <TodoFrom onSubmit = {addTodo} /> 
+           <TodoFrom onSubmit = {addTodo} />
+           <Todo 
+           todos={todos}
+           completeTodo={completeTodo}
+           removeTodo={removeTodo}
+           editTodo={editTodo}
+           /> 
         </div>
     )
 }
